10ce803f86942db04d36505b6ad45cdab1e58960
Sheriff26 <students.amikom.ac.id>
2018-10-04 12:36:31 +0700

    Samitex terbaru

--- Samitex/app/src/main/java/com/kreasikode/samitex/KonfirmasiActivity.java
+++ Samitex/app/src/main/java/com/kreasikode/samitex/KonfirmasiActivity.java
@@ -1,28 +1,65 @@
 package com.kreasikode.samitex;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class KonfirmasiActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnSimpanKonfirmasi;
     private ImageView btnBackScanData;
+    private TextView tvMesin;
+    private TextView tvTanggalShift;
+    private TextView tvGroupOperator;
+    private TextView tvCounter;
+    private TextView tvCatatan;
+    private ImageView konfirmFoto;
+
+    private String Tanggal;
+    private String Shift;
+    private String Group;
+    private String Operator;
+    private String Mesin;
+    private String Counter;
+    private String Catatan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konfirmasi);
 
+        btnBackScanData = findViewById(R.id.back_icon);
+        btnBackScanData.setOnClickListener(this);
+        tvMesin = findViewById(R.id.tv_kode_mesin);
+        tvTanggalShift = findViewById(R.id.tv_konfirmasi_tanggal);
+        tvGroupOperator = findViewById(R.id.tv_nama_operator);
+        tvCounter = findViewById(R.id.tv_jumlah_couter);
+        tvCatatan = findViewById(R.id.tv_konfirmasi_catatan);
+        konfirmFoto = findViewById(R.id.img_bukti_photo);
+
         btnSimpanKonfirmasi = findViewById(R.id.btn_simpan);
         btnSimpanKonfirmasi.setOnClickListener(this);
 
-        btnBackScanData = findViewById(R.id.back_icon);
-        btnBackScanData.setOnClickListener(this);
+        Tanggal = getIntent().getExtras().getString("Tanggal");
+        Shift = getIntent().getExtras().getString("Shift");
+        Group = getIntent().getExtras().getString("Group");
+        Operator = getIntent().getExtras().getString("Operator");
+        Mesin = getIntent().getExtras().getString("Mesin");
+        Counter = getIntent().getExtras().getString("Counter");
+        Catatan = getIntent().getExtras().getString("Catatan");
+
+        tvMesin.setText(Mesin);
+        tvTanggalShift.setText(Tanggal + " / " +Shift);
+        tvGroupOperator.setText(Group + " - " +Operator);
+        tvCounter.setText(Counter);
+        tvCatatan.setText(Catatan);
 
     }
 
--- Samitex/app/src/main/java/com/kreasikode/samitex/ScanDataActivity.java
+++ Samitex/app/src/main/java/com/kreasikode/samitex/ScanDataActivity.java
@@ -1,11 +1,12 @@
 package com.kreasikode.samitex;
 
-import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,65 +20,98 @@ import android.widget.ImageView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
+import java.io.ByteArrayOutputStream;
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class ScanDataActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView imgScanner;
-    private Button btnSimpanData;
     private ImageView btnBackDashboard;
-    private ImageView tampilFoto;
     private EditText etTanggal;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private EditText etShift;
+    private EditText etGroup;
+    private EditText etOperator;
+    private EditText etMesin;
+    private EditText etCounter;
+    private EditText etCatatan;
+    private ImageView imgScanner;
+    private ImageView tampilFoto;
     private ImageButton btnTakePicture;
+    private Button btnSimpanData;
+
+    static final int REQUEST_IMAGE_CAPTURE = 26;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_data);
 
+        btnBackDashboard = findViewById(R.id.back_icon);
+        btnBackDashboard.setOnClickListener(this);
+
         etTanggal = findViewById(R.id.et_tanggal);
-        etTanggal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        ScanDataActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-                        mDateSetListener,
-                        year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                month = month + 1;
-                String date = day + " / " + month + " / " + year;
-                etTanggal.setText(date);
-            }
-        };
-
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        etTanggal.setOnClickListener(this);
+        etShift = findViewById(R.id.et_shift);
+        etGroup = findViewById(R.id.et_group);
+        etOperator = findViewById(R.id.et_operator);
+        etMesin = findViewById(R.id.et_mesin);
+        etCounter = findViewById(R.id.et_jumlah_counter);
+        etCatatan = findViewById(R.id.et_catatan);
 
         imgScanner = findViewById(R.id.img_scanner);
         imgScanner.setOnClickListener(this);
-        btnSimpanData = findViewById(R.id.btn_simpan_data);
-        btnSimpanData.setOnClickListener(this);
-        btnBackDashboard = findViewById(R.id.back_icon);
-        btnBackDashboard.setOnClickListener(this);
         btnTakePicture = findViewById(R.id.img_btn_take_photo);
         btnTakePicture.setOnClickListener(this);
-
+        btnSimpanData = findViewById(R.id.btn_simpan_data);
+        btnSimpanData.setOnClickListener(this);
         tampilFoto = findViewById(R.id.tampil_foto);
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            tampilFoto.setImageBitmap(imageBitmap);
+        }
+
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dpd = new DatePickerDialog(getActivity(),
+                    AlertDialog.THEME_HOLO_LIGHT,this,year,month,day);
+            return  dpd;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day){
+
+            EditText etTanggal = (EditText) getActivity().findViewById(R.id.et_tanggal);
+
+            // Create a Date variable/object with user chosen date
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(0);
+            cal.set(year, month, day, 0, 0, 0);
+            Date chosenDate = cal.getTime();
+
+            // Format the date using style full
+            DateFormat df_full = DateFormat.getDateInstance(DateFormat.FULL);
+            String df_full_str = df_full.format(chosenDate);
+            // Display the formatted date
+            etTanggal.setText(df_full_str);
+        }
     }
 
     @Override
@@ -93,11 +127,6 @@ public class ScanDataActivity extends AppCompatActivity implements View.OnClickL
                 scannerIntegrator.initiateScan();
                 break;
 
-            case R.id.btn_simpan_data:
-                Intent simpanDataIntent = new Intent(ScanDataActivity.this, KonfirmasiActivity.class);
-                startActivity(simpanDataIntent);
-                break;
-
             case R.id.back_icon:
                 Intent backDashboardIntent = new Intent(ScanDataActivity.this, DashboardActivity.class);
                 backDashboardIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -106,21 +135,40 @@ public class ScanDataActivity extends AppCompatActivity implements View.OnClickL
 
             case R.id.img_btn_take_photo:
                 Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePhotoIntent, 0);
+                if (takePhotoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+                }
+                break;
+
+            case R.id.et_tanggal:
+                DialogFragment dFragment = new DatePickerFragment();
+                dFragment.show(getFragmentManager(), "Date Picker");
+                break;
+
+            case R.id.btn_simpan_data:
+                String Tanggal = etTanggal.getText().toString();
+                String Shift = etShift.getText().toString();
+                String Group = etGroup.getText().toString();
+                String Operator = etOperator.getText().toString();
+                String Mesin = etMesin.getText().toString();
+                String Counter = etCounter.getText().toString();
+                String Catatan = etCatatan.getText().toString();
+
+                Intent simpanDataIntent = new Intent(ScanDataActivity.this, KonfirmasiActivity.class);
+                simpanDataIntent.putExtra("Tanggal", Tanggal);
+                simpanDataIntent.putExtra("Shift", Shift);
+                simpanDataIntent.putExtra("Group", Group);
+                simpanDataIntent.putExtra("Operator", Operator);
+                simpanDataIntent.putExtra("Mesin", Mesin);
+                simpanDataIntent.putExtra("Counter", Counter);
+                simpanDataIntent.putExtra("Catatan", Catatan);
+
+                //ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                //compress(Bitmap.CompressFormat.PNG,50,bs);
+                //simpanDataIntent.putExtra("ImageBitmap", bs.toByteArray());
+                startActivity(simpanDataIntent);
+                break;
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case 0:
-                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                    tampilFoto.setImageBitmap(bitmap);
-                    break;
-            }
-        }
-
-    }
 }
